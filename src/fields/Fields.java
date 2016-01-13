45350e8cdfa7c1ddd75c4345f503f8739eead8fc
Dactaxx <dmpribak@gmail.com>
2016-01-12 22:18:09 -0500

    Added writing functionality and improved reading

--- src/fields/Fields.java
+++ src/fields/Fields.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -14,6 +15,17 @@ public class Fields {
 	public static void main(String[] args) throws IOException {
 		//Window.createWindow();
 		readData();
+		for(int i = 0; i < data.size(); i++) {
+			System.out.println(data.get(i).name + " - " + data.get(i).color);
+			
+		}
+		
+		writeData(Dillan)
+		
+		for(int i = 0; i < data.size(); i++) {
+			System.out.println(data.get(i).name + " - " + data.get(i).color);
+			
+		}
 		
 	}
 	
@@ -21,15 +33,20 @@ public class Fields {
 		Scanner in = new Scanner(new FileReader(new File("data/data.txt")));
 		while(in.hasNextLine()) {
 			String line = in.nextLine();
-			Person person = new Person();
-			switch(line) {
-				case "name" :
+			if(line.equals("#")) {
+				Person person = new Person();
 				person.name = in.nextLine();
 				data.add(person);
-				break;
+				
+			} else {
+				switch(line) {
+					case "color" :
+					data.getLast().name = in.nextLine();
+					break;
+		
+				}
 				
 			}
-			System.out.println(person.name);
 
 		}
 		
@@ -41,7 +58,26 @@ public class Fields {
 		person.color = color;
 		data.add(person);
 		
+		Scanner in = new Scanner(new FileReader(new File("data/data.txt")));
+		ArrayList<String> lnlist = new ArrayList<String>();
+		
+		while(in.hasNextLine()) {
+			lnlist.add(in.nextLine());
+			
+		}
+		
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("data/data.txt")));
+		
+		for(int i = 0; i < lnlist.size(); i++) {
+			writer.write(lnlist.get(i));
+			writer.newLine();
+			
+		}
+		writer.write("#");
+		writer.newLine();
+		writer.write(name);
+		writer.write("color");
+		writer.write(Double.toString(color));
 		
 	}
 	
