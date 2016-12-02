c67f7bdbd855f3c6f3ab221f60fb9332dd501704
Dactaxx <dmpribak@gmail.com>
2016-12-01 20:58:24 -0500

    Fixed everything credit to new programming mentor

--- src/fields/Fields.java
+++ src/fields/Fields.java
@@ -1,6 +1,6 @@
 package fields;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -14,18 +14,20 @@ public class Fields {
 	
 	public static void main(String[] args) throws IOException {
 		//Window.createWindow();
-		readData();
-		for(int i = 0; i < data.size(); i++) {
-			System.out.println(data.get(i).name + " - " + data.get(i).color);
+		
+		readFile("data/data.txt");
+		//readData();
+		/*for(int i = 0; i < data.size(); i++) {
+			System.out.println(data.get(i).getName() + " - " + data.get(i).getColor());
 			
 		}
 		
 
 		
 		for(int i = 0; i < data.size(); i++) {
-			System.out.println(data.get(i).name + " - " + data.get(i).color);
+			System.out.println(data.get(i).getName() + " - " + data.get(i).getColor());
 			
-		}
+		}*/
 		
 	}
 	
@@ -35,13 +37,13 @@ public class Fields {
 			String line = in.nextLine();
 			if(line.equals("#")) {
 				Person person = new Person();
-				person.name = in.nextLine();
+				person.setName(in.nextLine()) ;
 				data.add(person);
 				
 			} else {
 				switch(line) {
 					case "color" :
-					data.getLast().name = in.nextLine();
+					data.getLast().setName(in.nextLine());
 					break;
 		
 				}
@@ -52,10 +54,28 @@ public class Fields {
 		
 	}
 	
+  public static void readFile(String path) {
+	  try {
+		  
+		  BufferedReader reader = new BufferedReader(new FileReader(path));
+		  String line="";
+		  
+		  while((line = reader.readLine()) != null){
+			  if(line == "#") {
+				  
+			  }
+		  }
+		  
+	  } catch(Exception e){
+		  
+	  }
+  
+  }
+	
 	public static void writeData(String name, double color) throws IOException {
 		Person person = new Person();
-		person.name = name;
-		person.color = color;
+		person.setName(name);
+		person.setColor(color);
 		data.add(person);
 		
 		Scanner in = new Scanner(new FileReader(new File("data/data.txt")));
@@ -63,11 +83,11 @@ public class Fields {
 		
 		for(int i = 0; i < data.size(); i++) {
 			writelist.add("#");
-			writelist.add(data.get(i).name);
+			writelist.add(data.get(i).getName());
 			
-			if(data.get(i).color != -1) {
+			if(data.get(i).getColor() != -1) {
 				writelist.add("color");
-				writelist.add(Double.toString(data.get(i).color));
+				writelist.add(Double.toString(data.get(i).getColor()));
 				
 			}
 			
@@ -78,16 +98,16 @@ public class Fields {
 			
 		}
 		
-		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("data/data.txt")));
+		FileWriter writer = new FileWriter("data/data.txt");
 		
 		for(int i = 0; i < writelist.size(); i++) {
 			writer.write(writelist.get(i));
-			writer.newLine();
+			writer.write("\n");
 			
 		}
 		
 		writer.write("#");
-		writer.newLine();
+		writer.write("\n");
 		writer.write(name);
 		writer.write("color");
 		writer.write(Double.toString(color));
--- src/fields/Person.java
+++ src/fields/Person.java
@@ -1,14 +1,65 @@
 package fields;
 
 public class Person {
-	public String name;
-	public double color, introvert, outdoors;
+	private String name;
+	private double color, introvert, outdoors;
 	
 	public Person() {
-		this.color = -1;
-		this.introvert = -1;
-		this.outdoors = -1;
-		
+		super();
+		// TODO Auto-generated constructor stub
 	}
+
+
+	public Person(String name, double color, double introvert, double outdoors) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.introvert = introvert;
+		this.outdoors = outdoors;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getColor() {
+		return color;
+	}
+
+
+	public void setColor(double color) {
+		this.color = color;
+	}
+
+
+	public double getIntrovert() {
+		return introvert;
+	}
+
+
+	public void setIntrovert(double introvert) {
+		this.introvert = introvert;
+	}
+
+
+	public double getOutdoors() {
+		return outdoors;
+	}
+
+
+	public void setOutdoors(double outdoors) {
+		this.outdoors = outdoors;
+	}
+	
+	
+	
+	
 	
 }
